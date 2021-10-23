36a1571c346b932f6d9bcbad181b16f24f466780
Jincheng Li <mail@jincheng.li>
2021-10-23 16:17:56 -0400

    Make Bucket a trait, and implement it with Slot.

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,9 @@ where
     value: V,
 }
 
+// A physical hash slot storing an entry.
 #[derive(Clone, Debug)]
-struct Bucket<K, V>
+struct Slot<K, V>
 where
     K: Hash + Eq,
 {
@@ -25,30 +26,51 @@ where
     entry: Option<Entry<K, V>>,
 }
 
+// A logical hash bucket storing many entries.
+// TODO: Add a method for iterating over occupied hops.
+trait Bucket {
+    fn is_occupied(&self, hop: usize) -> bool;
+
+    fn set_occupied(&mut self, hop: usize, occupied: bool);
+
+    fn is_full(&self) -> bool;
+}
+
 #[derive(Clone, Debug)]
 pub struct HsHashMap<K, V>
 where
     K: Hash + Eq,
 {
-    buckets: Vec<Bucket<K, V>>,
+    slots: Vec<Slot<K, V>>,
     length: usize,
 }
 
-impl<K, V> Bucket<K, V>
+impl<K, V> Slot<K, V>
 where
     K: Hash + Eq,
 {
-    fn new() -> Bucket<K, V> {
-        Bucket {
+    fn new() -> Slot<K, V> {
+        Slot {
             hop_info: Bitmap::new(),
             entry: None,
         }
     }
 
-    // TODO: This is ambiguous. Is the slot itself vacant, or is the entire
-    // logical bucket vacant?
     fn is_vacant(&self) -> bool {
         self.entry.is_none()
+    }
+}
+
+impl<K, V> Bucket for Slot<K, V>
+where
+    K: Hash + Eq,
+{
+    fn is_occupied(&self, hop: usize) -> bool {
+        self.hop_info.get(hop)
+    }
+
+    fn set_occupied(&mut self, hop: usize, occupied: bool) {
+        self.hop_info.set(hop, occupied);
     }
 
     fn is_full(&self) -> bool {
@@ -57,20 +79,20 @@ where
 }
 
 // TODO: Add deletion.
-// TODO: Optimize this. I'm currently handicapped by WSL2, which doesn't have
-// hardware performance counters. `cargo flamegraph` has also been unhelpful -
-// it produces a mostly blank graph. I may need to build Rust from source in
-// order to get standard library functions to show up.
+// TODO: Optimize this. I'm currently handicapped by virtual machines, which
+// do not have hardware performance counters. `cargo flamegraph` has also been
+// unhelpful - it produces a mostly blank graph. I may need to build Rust from
+// source in order to get standard library functions to show up.
 impl<K, V> HsHashMap<K, V>
 where
     K: Hash + Eq,
 {
     // Public functions.
     pub fn new() -> HsHashMap<K, V> {
-        let mut buckets: Vec<Bucket<K, V>> = vec![];
-        buckets.resize_with(HOP_LIMIT, Bucket::new);
+        let mut slots: Vec<Slot<K, V>> = vec![];
+        slots.resize_with(HOP_LIMIT, Slot::new);
         HsHashMap {
-            buckets: buckets,
+            slots: slots,
             length: 0,
         }
     }
@@ -83,14 +105,14 @@ where
         }
         // If `k` isn't present, find the closest empty slot, and insert it.
         let bucket_idx = self.get_bucket(&k);
-        if self.buckets[bucket_idx].is_full() {
+        if self.slots[bucket_idx].is_full() {
             self.rehash(self.capacity() * 2);
             return self.insert(k, v);
         }
         let capacity = self.capacity();
         let vacant_hop = (0..capacity).find(|hop| {
             let entry_idx = (bucket_idx + hop) % capacity;
-            self.buckets[entry_idx].entry.is_none()
+            self.slots[entry_idx].entry.is_none()
         });
         if vacant_hop.is_none() {
             self.rehash(self.capacity() * 2);
@@ -109,24 +131,24 @@ where
                 }
             }
         }
-        let buckets = &mut self.buckets;
+        let slots = &mut self.slots;
         let entry_idx = (bucket_idx + hop) % capacity;
-        debug_assert!(buckets[entry_idx].is_vacant());
-        debug_assert!(!buckets[bucket_idx].hop_info.get(hop));
-        buckets[bucket_idx].hop_info.set(hop, true);
-        buckets[entry_idx].entry = Some(Entry { key: k, value: v });
+        debug_assert!(slots[entry_idx].is_vacant());
+        debug_assert!(!slots[bucket_idx].is_occupied(hop));
+        slots[bucket_idx].set_occupied(hop, true);
+        slots[entry_idx].entry = Some(Entry { key: k, value: v });
         self.length += 1;
         None
     }
 
     pub fn get(&self, k: &K) -> Option<&V> {
         let bucket_idx = self.get_bucket(&k);
-        let hop_info = &self.buckets[bucket_idx].hop_info;
+        let hop_info = &self.slots[bucket_idx].hop_info;
         for hop in (0..HOP_LIMIT).filter(|hop| hop_info.get(*hop)) {
             let entry_idx = (bucket_idx + hop) % self.capacity();
-            match &self.buckets[entry_idx].entry {
+            match &self.slots[entry_idx].entry {
                 None => {
-                    // TODO: Can we relax this such that, when deleting an 
+                    // TODO: Can we relax this such that, when deleting an
                     // item, we don't backtrack to reset all the bitmaps?
                     std::debug_assert!(false, "Bucket should not be empty.");
                     continue;
@@ -150,7 +172,7 @@ where
     }
 
     pub fn capacity(&self) -> usize {
-        self.buckets.len()
+        self.slots.len()
     }
 
     pub fn len(&self) -> usize {
@@ -158,8 +180,8 @@ where
     }
 
     pub fn clear(&mut self) {
-        self.buckets.clear();
-        self.buckets.resize_with(HOP_LIMIT, Bucket::new);
+        self.slots.clear();
+        self.slots.resize_with(HOP_LIMIT, Slot::new);
         self.length = 0;
     }
 
@@ -177,12 +199,12 @@ where
 
     fn rehash(&mut self, new_capacity: usize) {
         // println!("Rehashing from {} to {}.", self.capacity(), new_capacity);
-        let buckets = mem::replace(&mut self.buckets, vec![]);
+        let slots = mem::replace(&mut self.slots, vec![]);
         let original_length = self.length;
-        self.buckets.resize_with(new_capacity, Bucket::new);
+        self.slots.resize_with(new_capacity, Slot::new);
         self.length = 0;
-        for bucket in buckets {
-            if let Some(entry) = bucket.entry {
+        for slot in slots {
+            if let Some(entry) = slot.entry {
                 self.insert(entry.key, entry.value);
             }
         }
@@ -193,29 +215,27 @@ where
     fn move_closer(&mut self, origin_idx: usize, current_hop: usize) -> Option<usize> {
         debug_assert!(current_hop >= HOP_LIMIT);
         let current_idx = (origin_idx + current_hop) % self.capacity();
-        debug_assert!(self.buckets[current_idx].is_vacant());
+        debug_assert!(self.slots[current_idx].is_vacant());
         for candidate_hop in (current_hop - HOP_LIMIT + 1)..current_hop {
             let candidate_idx = (origin_idx + candidate_hop) % self.capacity();
             let candidate_to_current_delta = current_hop - candidate_hop;
             // Hop info for current_hop must be empty.
             debug_assert!(candidate_to_current_delta < HOP_LIMIT);
-            debug_assert!(!self.buckets[candidate_idx]
+            debug_assert!(!self.slots[candidate_idx]
                 .hop_info
                 .get(candidate_to_current_delta));
             // Find the earliest slot for `entry_idx` that isn't empty.
-            if let Some(victim_hop) = self.buckets[candidate_idx].hop_info.first_index() {
+            if let Some(victim_hop) = self.slots[candidate_idx].hop_info.first_index() {
                 if victim_hop >= candidate_to_current_delta {
                     continue;
                 }
                 // Swap the victim entry with the empty slot.
                 let victim_idx = (candidate_idx + victim_hop) % self.capacity();
-                let victim_entry = mem::take(&mut self.buckets[victim_idx].entry);
-                debug_assert!(self.buckets[victim_idx].is_vacant());
-                self.buckets[current_idx].entry = victim_entry;
-                self.buckets[candidate_idx].hop_info.set(victim_hop, false);
-                self.buckets[candidate_idx]
-                    .hop_info
-                    .set(candidate_to_current_delta, true);
+                let victim_entry = mem::take(&mut self.slots[victim_idx].entry);
+                debug_assert!(self.slots[victim_idx].is_vacant());
+                self.slots[current_idx].entry = victim_entry;
+                self.slots[candidate_idx].set_occupied(victim_hop, false);
+                self.slots[candidate_idx].set_occupied(candidate_to_current_delta, true);
                 // println!(
                 //     "Moved an empty slot from hop/index {}/{} to {}/{}.",
                 //     current_hop,
@@ -231,10 +251,10 @@ where
 
     #[cfg(test)]
     fn check_consistency(&self) -> bool {
-        for (bucket_idx, bucket) in self.buckets.iter().enumerate() {
+        for (bucket_idx, bucket) in self.slots.iter().enumerate() {
             for hop in (0..HOP_LIMIT).filter(|hop| bucket.hop_info.get(*hop)) {
                 let entry_idx = (bucket_idx + hop) % self.capacity();
-                if self.buckets[entry_idx].is_vacant() {
+                if self.slots[entry_idx].is_vacant() {
                     println!(
                         "Bucket {} says hop {} / entry {} is occupied, but it's vacant.",
                         bucket_idx, hop, entry_idx,
